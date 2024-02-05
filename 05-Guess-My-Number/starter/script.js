1ff2ccff3efd93f2af7e3d8c1be88b6786579c66
unknown <josemari94@gmail.com>
2024-02-05 15:10:43 +0100

    Start game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    btnCheck = document.querySelector('.btn.check');
+    btnAgain = document.querySelector('.btn.again');
+    numberElement = document.querySelector('.number');  
+    messageElement = document.querySelector('.message');
+    scoreElement = document.querySelector('.score');
+    highscoreElement = document.querySelector('.highscore');
+    guessElement = document.querySelector('.guess');
+
+    let minNumber = 1;
+    let maxNumber = 20;
+
+    function guessNumber() {
+        const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+        numberElement.textContent = randomNumber;
+        let newNumber = numberElement.textContent;
+        console.log(newNumber);  
+    }
+
+    guessNumber();
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    })
+    
+
+
+
+
