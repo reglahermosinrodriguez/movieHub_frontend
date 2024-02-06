54433230a1fabd95972c65ba3deac02f18a62e8e
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-02-06 15:00:10 +0100

    Start logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,51 +1,49 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-    btnCheck = document.querySelector('.btn.check');
-    btnAgain = document.querySelector('.btn.again');
-    numberElement = document.querySelector('.number');  
-    messageElement = document.querySelector('.message');
-    scoreElement = document.querySelector('.score');
-    highscoreElement = document.querySelector('.highscore');
-    guessElement = document.querySelector('.guess');
+    const btnCheck = document.querySelector('.btn.check');
+    const btnAgain = document.querySelector('.btn.again');
+    const numberElement = document.querySelector('.number');  
+    const messageElement = document.querySelector('.message');
+    const scoreElement = document.querySelector('.score');
+    const highscoreElement = document.querySelector('.highscore');
+    const guessElement = document.querySelector('.guess');
 
     let minNumber = 1;
     let maxNumber = 20;
 
-    function guessNumber() {
+    function guessNumber(inputNumber) {
         const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
         numberElement.textContent = randomNumber;
-        let newNumber = numberElement.textContent;
-        console.log(newNumber);  
+        console.log(randomNumber);  
+
+        if (inputNumber < randomNumber) {
+            return alert("El número es mayor!");
+
+        } else if (inputNumber > randomNumber) {
+            return alert("El número es menor!");
+
+        } else {
+            return alert("Congratulations!!🍀")
+        };
+}
+        btnCheck.addEventListener('click', function () {
+            const inputNumber = parseInt(guessElement.value)
+            sendNumber(inputNumber);
+        
+    });
+
+        btnAgain.addEventListener('click', function () {
+        // alert("Again!");
+        
+        
+    
+});
+
+    function sendNumber(inputNumber) {
+        console.log(inputNumber);
+        guessNumber(inputNumber)
+        
     }
 
-    guessNumber();
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    })
-    
-
-
-
-
+});
