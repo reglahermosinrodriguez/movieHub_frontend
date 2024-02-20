61b08cad62311a24a3c51896498371ded55f0783
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-02-20 13:00:47 +0100

    Products

--- Desktop/Assembler/individual projects/módulo 3/ecommerce/src/pages/home/index.tsx
+++ Desktop/Assembler/individual projects/módulo 3/ecommerce/src/pages/home/index.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Products } from '../../interfaces/products'
 
-type Props = {}
 
-export default function Home({}: Props) {
-  return (
-    <h1>Hola, </h1>
-  )
+
+export default function Home() {
+  const [productData, setProductData] = useState<Products[]>([]);
+
+async function getProducts() {
+  try {
+    const data = await fetch('src/assets/data/products.json')
+    const JSONdata = await data.json()
+    setProductData(JSONdata);
+  } catch (error) {
+    console.log(error)
+    
+  }
 }
+
+useEffect(() => {
+  getProducts();
+}, [])
+
+
+  return(
+    <>
+    <div>
+      {productData.map((product) => {
+        return(
+          <div key={product.id}>
+            <h1>{product.Name}</h1>
+            <img src={product.image} alt={product.Name}/>
+            <p>{product.price}</p>
+            <p>{product.description}</p>
+
+          </div>
+
+        );
+
+
+      })}
+    </div>
+    </>
+
+  )
+} 
