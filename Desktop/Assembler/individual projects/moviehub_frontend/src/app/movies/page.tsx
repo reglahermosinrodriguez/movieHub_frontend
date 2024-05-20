2420e5f538824551a53af0ee90f66e6f581c1e54
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-05-20 19:57:45 +0200

    Add styles

--- Desktop/Assembler/individual projects/moviehub_frontend/src/app/movies/page.tsx
+++ Desktop/Assembler/individual projects/moviehub_frontend/src/app/movies/page.tsx
@@ -8,16 +8,16 @@ export default async function movies() {
 return(
 <>
 
-<h1>MOVIES</h1>
-<div>
+<h1 className="title">MOVIES</h1>
+<div className="container">
     {getMovies?.data.map((event:Movies) => (
-        <div key={event.id}>
+        <div className="card" key={event.id}>
         <h2>{event.name}</h2>
         <img src={event.image} alt={`Cartel de ${event.name}`}/>
         <p>Genre: {event.genre.join(', ')}</p>
         <p>Sinopsis: {event.sinopsis}</p>
         <p>Score: {event.score}</p>
-        <Link href={String(event.id)}><button>{event.id}</button></Link>
+        <Link href={String(event.id)}><p>{event.id}</p></Link>
         </div>
 
 
--- Desktop/Assembler/individual projects/moviehub_frontend/src/app/page.tsx
+++ Desktop/Assembler/individual projects/moviehub_frontend/src/app/page.tsx
@@ -5,12 +5,14 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>
-        <h1>YOUR MOVIES APP</h1>
-        <Link href="/movies"><button>Go to movies</button></Link>
-
-
-
+    <>
+    <h1 className="title">YOUR MOVIES APP</h1>   
+   
+    <div className="title-button"><Link href="/movies" ><button>Go to movies</button></Link>
     </div>
+
+    </>
+
+
   )
 }
